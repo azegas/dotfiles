12f15471c119def5038c7d09eff750a5914ffe04
arvydasg <azegaspa@gmail.com>
2022-02-07 20:47:04 +0200

    feat: added files needed for a fresh installation

---
+++ for installation/init.el
@@ -0,0 +1,17 @@
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/Dropbox/emacs/myinit.org"))
