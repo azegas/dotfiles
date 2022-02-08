48540ff5a651e935584b799929e060cbb218cdca
arvydasg <azegaspa@gmail.com>
2022-02-08 10:14:57 +0200

    feat: added dumb-jump package for quick definition findability

--- myinit.el
+++ myinit.el
@@ -284,3 +284,15 @@ neo-force-change-root t)
     (add-to-list 'beacon-dont-blink-major-modes 'term-mode)
 
     (beacon-mode 1)))
+
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config 
+  (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+:init
+(dumb-jump-mode)
+  :ensure
+)
