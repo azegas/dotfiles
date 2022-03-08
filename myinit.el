340ba166b0183ce7e01872b95588db6e862d4cd3
arvydasg <azegaspa@gmail.com>
2022-03-08 22:01:29 +0200

    feat: most recent changes

--- myinit.el
+++ myinit.el
@@ -26,6 +26,9 @@
 (time-subtract after-init-time before-init-time)))
 gcs-done))
 
+;; no more annoying buffer list combinatios
+(global-set-key (kbd "C-x C-b") 'switch-to-buffer)
+
 (add-hook 'emacs-startup-hook #'efs/display-startup-time)
 
 ;; writes parens automatically for you
@@ -69,8 +72,8 @@ gcs-done))
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+;; (add-hook 'text-mode-hook 'flyspell-mode)
+;; (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 ;; start wrapping at 80 characterers
 (setq fill-column 80)
@@ -152,9 +155,7 @@ gcs-done))
 ;; described in the line above and forget about it. Refiling will
 ;; work, agenda will work.  if your org agenda files are not there,
 ;; do C-c C-e on the parentheses below. Evaluate them.
-
 (setq org-agenda-files '("~/Dropbox/documents/org/"))
-
 ;; Stop preparing agenda buffers on startup
 (setq org-agenda-inhibit-startup t)
 ;; when you press C-c C-z on a headline, it makes a note. Specifying the name of that drawyer.
@@ -183,7 +184,27 @@ gcs-done))
 ;; agenda first and only then org-clock-goto.
 ;; C-h k - and writing C-c C-x C-j was very useful. Got name of the key.
 (global-set-key (kbd "C-c C-x C-j") 'org-clock-goto)
-
+;; heading indentation
+;; do M-x revert-buffer if the changes doesn't appear. Should indent then
+(setq org-startup-indented t)
+;; RET to follow links
+(setq org-return-follows-link t)
+;; Prevent setting "done" on he heading if subheadings are not completed
+(setq org-enforce-todo-dependencies t)
+;; Prefix tasks with parent heading Instead of showing the filename
+;; where the task resides, I show the first characters of the parent
+;; heading. That way I can use short and generic task names and still
+;; understand it in the agenda. No need to repeat the context in the
+;; task name anymore.
+(defun getlasthead ()
+(let ((x (nth 0 (last (org-get-outline-path)))))
+(if x
+(if (> (string-width x) 12)
+(concat "[" (org-format-outline-path (list (substring x 0 12))) "]")
+(concat "[" (org-format-outline-path (list x)) "]"))
+"")))
+(setq org-agenda-prefix-format " %i %-15(getlasthead)%?-12t% s ")
+;; keywords for org mode
 (setq org-todo-keywords
 (quote ((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(p)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))))
 
@@ -193,47 +214,60 @@ gcs-done))
 
     ;; to see all the emacs predifined colors - M-x list-colors-display
     (setq org-todo-keyword-faces
-	     (quote (("TODO" :foreground "IndianRed1" :weight bold)
-		     ("NEXT" :foreground "DeepSkyBlue2" :weight bold)
-		     ("IN-PROGRESS" :foreground "gold1" :weight bold)
-		     ("DONE" :foreground "forest green" :weight bold))))
+             (quote (("TODO" :foreground "IndianRed1" :weight bold)
+                     ("NEXT" :foreground "DeepSkyBlue2" :weight bold)
+                     ("IN-PROGRESS" :foreground "gold1" :weight bold)
+                     ("DONE" :foreground "forest green" :weight bold))))
 
-;; ;; Bieber agenda STARTS HERE
+;; ;; dont show habit tasks in "all todos" list.
+;;     (defun air-org-skip-subtree-if-habit ()
+;;       "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+;;       (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+;;         (if (string= (org-entry-get nil "STYLE") "habit")
+;;             subtree-end
+;;           nil)))
 
-;; dont show habit tasks in "all todos" list.
-  (defun air-org-skip-subtree-if-habit ()
-    "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
-    (let ((subtree-end (save-excursion (org-end-of-subtree t))))
-      (if (string= (org-entry-get nil "STYLE") "habit")
-	  subtree-end
-	nil)))
+;; ;; defining a function to skip the tasks with priorities in the "all todo's list"
+;;       (defun air-org-skip-subtree-if-priority (priority)
+;;       "Skip an agenda subtree if it has a priority of PRIORITY.
 
-	;; defining a function to skip the tasks wiht priorities in the "all todo's list"
-    (defun air-org-skip-subtree-if-priority (priority)
-    "Skip an agenda subtree if it has a priority of PRIORITY.
+;;            PRIORITY may be one of the characters ?A, ?B, or ?C."
+;;              (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+;;                    (pri-value (* 1000 (- org-lowest-priority priority)))
+;;                    (pri-current (org-get-priority (thing-at-point 'line t))))
+;;                (if (= pri-value pri-current)
+;;                    subtree-end
+;;                  nil)))
 
-	 PRIORITY may be one of the characters ?A, ?B, or ?C."
-	   (let ((subtree-end (save-excursion (org-end-of-subtree t)))
-		 (pri-value (* 1000 (- org-lowest-priority priority)))
-		 (pri-current (org-get-priority (thing-at-point 'line t))))
-	     (if (= pri-value pri-current)
-		 subtree-end
-	       nil)))
+;;   ;; Final agenda view look
+;;   (setq org-agenda-custom-commands
+;;         '(("a" "Daily agenda and all TODOs"
+;;            ((tags "PRIORITY=\"A\""
+;;                   ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+;;                    (org-agenda-overriding-header "High-priority unfinished tasks:")))
+;;             (agenda "" ((org-agenda-span 3)))
+;;             (alltodo ""
+;;                      ((org-agenda-skip-function '(or (air-org-skip-subtree-if-habit)
+;;                                                      (air-org-skip-subtree-if-priority ?A)
+;;                                                      (org-agenda-skip-if nil '(scheduled deadline))))
+;;                       (org-agenda-overriding-header "ALL normal priority tasks:")))))))
 
-;; Final agenda view look
-(setq org-agenda-custom-commands
-      '(("a" "Daily agenda and all TODOs"
-	 ((tags "PRIORITY=\"A\""
-		((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
-		 (org-agenda-overriding-header "High-priority unfinished tasks:")))
-	  (agenda "" ((org-agenda-span 3)))
-	  (alltodo ""
-		   ((org-agenda-skip-function '(or (air-org-skip-subtree-if-habit)
-						   (air-org-skip-subtree-if-priority ?A)
-						   (org-agenda-skip-if nil '(scheduled deadline))))
-		    (org-agenda-overriding-header "ALL normal priority tasks:")))))))
+;; Show agenda + started tasks with "waiting" label
+      (setq org-agenda-custom-commands
+            '(("a" "Daily agenda and all TODOs"
+               ((agenda "" ((org-agenda-span 3)))
+               (tags-todo "/+WAITING"
+                      ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                       (org-agenda-overriding-header "Started tasks:")))))))
 
-;; ;; Bieber agenda FINISHES HERE
+;; Show completed tasks
+  (add-to-list 'org-agenda-custom-commands
+               '("f" "Finished tasks only DONE tasks"
+                 agenda ""
+                 ((org-agenda-start-on-weekday 1)
+                  (org-agenda-start-with-log-mode '(closed))
+                  (org-agenda-skip-function '(org-agenda-skip-entry-if 'notregexp "^\\*\\* DONE ")))))
+;; (org-agenda-archives-mode t)
 
 (use-package org-habit
   :ensure nil
@@ -242,16 +276,22 @@ gcs-done))
 (setq org-habit-graph-column 72) ;push little further to the rigth
 
 (setq org-log-note-clock-out t)
-;; Clock out when moving task to a done state
-(setq org-clock-out-when-done t)
-;; Resume clocking task when emacs is restarted
-(org-clock-persistence-insinuate)
-;; Save the running clock and all clock history when exiting Emacs, load it on startup
-(setq org-clock-persist t)
-;; Resume clocking task on clock-in if the clock is open
-(setq org-clock-in-resume t)
-;; Do not prompt to resume an active clock, just resume it
-(setq org-clock-persist-query-resume nil)
+  ;; Clock out when moving task to a done state
+  (setq org-clock-out-when-done t)
+  ;; Resume clocking task when emacs is restarted
+  (org-clock-persistence-insinuate)
+  ;; Save the running clock and all clock history when exiting Emacs, load it on startup
+  (setq org-clock-persist t)
+  ;; Resume clocking task on clock-in if the clock is open
+  (setq org-clock-in-resume t)
+  ;; Do not prompt to resume an active clock, just resume it
+  (setq org-clock-persist-query-resume nil)
+
+;; clocktable example
+;; displays weekdays
+;; #+BEGIN: clocktable :maxlevel 3 :scope file :step day :tstart "<-1w>" :tend "<now>" :compact t
+;; displays month
+;; #+BEGIN: clocktable :maxlevel 5 :compact nil :emphasize t :scope subtree :timestamp t :link t :header "#+NAME: 2022_Vasaris\n"
 
 ;; To create an estimate for a task or subtree start column mode with C-c C-x C-c and collapse the tree with c
 ; Set default column view headings: Task Effort Clock_Summary
@@ -266,22 +306,22 @@ gcs-done))
 
 ;; useful org-capture document - https://orgmode.org/manual/Template-expansion.html
 ;; setting up the templates for c-c c
+;; genius. that effort.
 (define-key global-map "\C-cc" 'org-capture)
 (setq org-capture-templates '(
-("a" "Arvydas.dev" entry (file+headline "~/Dropbox/documents/org/arvydasdev.org" "arvydas.dev") "* TODO %?\n%U%^{Effort}p")
-("e" "Emacs" entry (file+headline "~/Dropbox/documents/org/src_emacs.org" "Emacs") "* TODO %?\n%U%^{Effort}p")
-("s" "Smuti Fruti" entry (file+headline "~/Dropbox/documents/org/src_smutifruti.org" "Smuti Fruti") "* TODO %?\n%U%^{Effort}p")
-("f" "Facebook_django" entry (file+headline "~/Dropbox/documents/org/src_facebook_django.org" "Facebook_django") "* TODO %?\n%U%^{Effort}p")
-("p" "Personal" entry (file+headline "~/Dropbox/documents/org/personal.org" "Personal") "* TODO %?\n%U%^{Effort}p")
-("d" "Diary" entry (file+datetree "~/Dropbox/documents/org/notes/diary.org" "Diary") "* %U %^{Title}\n%?")
-("r" "Refile" entry (file+headline "~/Dropbox/documents/org/refile.org" "Refile")"* TODO %?\n%U%^{Effort}p"))) ;; genius. that effort.
+("a" "Arvydas.dev" entry (file+headline "~/Dropbox/documents/org/arvydasdev.org" "arvydas.dev") "* TODO %?\n%^{Effort}p")
+("e" "Emacs" entry (file+headline "~/Dropbox/documents/org/src_emacs.org" "Emacs") "* TODO %?\n%^{Effort}p")
+("s" "Smuti Fruti" entry (file+headline "~/Dropbox/documents/org/src_smutifruti.org" "Smuti Fruti") "* TODO %?\n%^{Effort}p")
+("f" "Facebook_django" entry (file+headline "~/Dropbox/documents/org/src_facebook_django.org" "Facebook_django") "* TODO %?\n%^{Effort}p")
+("p" "Personal" entry (file+headline "~/Dropbox/documents/org/personal.org" "Personal") "* TODO %?\n%^{Effort}p")
+("d" "Diary" entry (file+datetree "~/Dropbox/documents/org/notes/diary.org" "Diary") "* %U %^{Title}\n%?")))
 ;; ("p" "Planned" entry (file+headline "~/Dropbox/1.planai/tickler.org" "Planned") "* %i%? %^{SCHEDULED}p" :prepend t)
 ;; ("r" "Repeating" entry (file+headline "~/Dropbox/1.planai/tickler.org" "Repeating") "* %i%? %^{SCHEDULED}p")))
 
 (use-package ace-window
       :ensure t
       :init (setq aw-keys '(?q ?w ?e ?r ?y ?h ?j ?k ?l)
-  ;		aw-ignore-current t ; not good to turn off since I wont be able to do c-o o <current>
+      ;aw-ignore-current t ; not good to turn off since I wont be able to do c-o o <current>
                   aw-dispatch-always t)
       :bind (("C-x o" . ace-window)
              ("M-O" . ace-swap-window)
@@ -445,15 +485,41 @@ gcs-done))
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (setq elpy-rpc-python-command "python3")
-(setq python-shell-interpreter "python3")
-(setq elpy-get-info-from-shell t)
-(use-package elpy
+  (setq python-shell-interpreter "python3")
+  (setq elpy-get-info-from-shell t)
+  (use-package elpy
+    :ensure t
+    :custom (elpy-rpc-backend "jedi")
+    :init
+    (elpy-enable))
+    ;; :bind (("M-." . elpy-goto-definition)))
+    ;; (setq elpy-rpc-virtualenv-path 'current)
+    (set-language-environment "UTF-8")
+
+;; (use-package elpy
+;;   :init
+;;   (elpy-enable)
+;;   :config
+;;   (setq python-shell-interpreter "python3"
+;;         python-shell-interpreter-args "-i --simple-prompt")
+;;   (add-hook 'python-mode-hook 'eldoc-mode)
+;;   (setq elpy-rpc-python-command "python3")
+;;   (setq elpy-shell-echo-output nil)
+;;   (setq python-shell-completion-native-enable nil)
+;;   (setq elpy-rpc-backend "jedi")
+;;   (setq python-indent-offset 4
+;;         python-indent 4))
+
+(use-package company-quickhelp
   :ensure t
-  ;; :custom (elpy-rpc-backend "jedi")
-  :init
-  (elpy-enable))
-  (setq elpy-rpc-virtualenv-path 'current)
-  (set-language-environment "UTF-8")
+  :config
+  (company-quickhelp-mode 1)
+  (eval-after-load 'company
+  '(define-key company-active-map (kbd "C-c h") #'company-quickhelp-manual-begin)))
+  (setq company-quickhelp-delay 0)
+
+  ;; (setq pos-tip-foreground-color "#FFFFFF"
+  ;; pos-tip-background-color "#FFF68F")
 
 ;; (defun my-python-line ()
 ;;  (interactive)
@@ -476,10 +542,11 @@ gcs-done))
 ;;  '(define-key elpy-mode-map (kbd "C-c <C-return>") 'my-python-line))
 
 (use-package emmet-mode
-:ensure t
-:config
+  :ensure t
+  :config)
+(add-hook 'web-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
-(add-hook 'css-mode-hook  'emmet-mode)) ;; enable Emmet's css abbreviation.
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
 (use-package web-mode
     :ensure t
@@ -498,21 +565,63 @@ gcs-done))
 
 (use-package multiple-cursors
   :ensure t
-  :bind (("M-." . mc/mark-next-like-this)
-         ("M-," . mc/unmark-next-like-this)
-         ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
+  :bind (("C-c m" . mc/mark-next-like-this)
+         ("C-c u" . mc/unmark-next-like-this)))
 
-(use-package fill-column-indicator
-  :ensure t)
+;; (use-package fill-column-indicator
+;;   :ensure t)
 
-(setq fci-rule-width 1)
-(setq fci-rule-column 80)
+;; (setq fci-rule-width 1)
+;; (setq fci-rule-column 80)
 
-(add-hook 'text-mode-hook #'fci-mode)
-(add-hook 'prog-mode-hook #'fci-mode)
+;; (add-hook 'text-mode-hook #'fci-mode)
+;; (add-hook 'prog-mode-hook #'fci-mode)
 
-(use-package smooth-scroll
-   :ensure t)
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-palenight t))
+
+(use-package doom-modeline
+    :ensure t
+    :init (doom-modeline-mode 1))
+
+(set-face-attribute 'mode-line nil
+                    :background "#353644"
+                    :foreground "white"
+                    :box '(:line-width 2 :color "#353644")
+                    :overline nil
+                    :underline nil)
+
+(set-face-attribute 'mode-line-inactive nil
+                    :background "#565063"
+                    :foreground "white"
+                    :box '(:line-width 2 :color "#565063")
+                    :overline nil
+                    :underline nil)
+
+(use-package goto-chg
+      :ensure t)
+(global-set-key [(control ?.)] 'goto-last-change)
+(global-set-key [(control ?,)] 'goto-last-change-reverse)
+
+;;;============================================================================
+;;;
+;;; Build interface to look how I want it to look
+;;; what about a new line
+;;;
+;;;============================================================================
+
+;; (use-package smooth-scroll
+;;    :ensure t)
+;; (smooth-scroll-mode t)
+;; (global-set-key [(control  down)]  'scroll-up-1)
+;; (global-set-key [(control  up)]    'scroll-down-1)
+;; (global-set-key [(control  left)]  'scroll-right-1)
+;; (global-set-key [(control  right)] 'scroll-left-1)
 
 ;; (use-package smooth-scrolling
 ;;   :ensure t)
@@ -559,20 +668,6 @@ gcs-done))
 ;; (setq lsp-headerline-breadcrumb-enable nil)
 ;; (setq lsp-ui-doc-enable nil)
 ;; (setq lsp-completion-enable-additional-text-edit nil)
-
-;; (use-package doom-themes
-;;   :ensure t
-;;   :config
-;;   ;; Global settings (defaults)
-;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;;   (load-theme 'doom-palenight t)
-;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   (doom-themes-org-config))
-
-;; (use-package doom-modeline
-;;   :ensure t
-;;   :init (doom-modeline-mode 1))
 
 ;; (use-package dumb-jump
 ;;   :bind (("M-g o" . dumb-jump-go-other-window)
