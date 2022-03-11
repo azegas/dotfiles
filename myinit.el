63dec2046293ba519fc26bef86ddbba10150a43d
arvydasg <azegaspa@gmail.com>
2022-03-11 17:47:57 +0200

    feat: yes

--- myinit.el
+++ myinit.el
@@ -109,6 +109,24 @@ gcs-done))
 ;; test
 (setq-default indent-tabs-mode nil)
 
+;; ;; Initialize package sources
+;; (require 'package)
+
+;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;;                          ("org" . "https://orgmode.org/elpa/")
+;;                          ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; (package-initialize)
+;; (unless package-archive-contents
+;;  (package-refresh-contents))
+
+
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
+;; (org-babel-load-file (expand-file-name "~/Dropbox/src/emacs/myinit.org"))
+
 (use-package company
   :ensure t
   :config
