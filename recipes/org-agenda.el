0ccc5b76a41093793bf395ba823985f9523515f2
arvydasg <azegaspa@gmail.com>
2022-04-25 07:19:20 +0300

    feat: a few agenda view and settings changes

--- recipes/org-agenda.el
+++ recipes/org-agenda.el
@@ -6,6 +6,8 @@
 ;; described in the line above and forget about it. Refiling will
 ;; work, agenda will work.  if your org agenda files are not there,
 ;; do C-c C-e on the parentheses below. Evaluate them.
+
+;; advanced searching - https://orgmode.org/worg/org-tutorials/advanced-searching.html
 ;;; Code:
 
 (setq org-agenda-custom-commands
@@ -15,7 +17,8 @@
         ("p" "pkc"
           ((agenda "" ((org-agenda-span 7)
                        (org-agenda-tag-filter-preset '("+pkc"))))))
-        ("t" "pkc tasks" tags-todo "+pkc")))
+        ("k" "pkc tasks" tags-todo "+pkc")
+        ("z" "effort stuff" tags-todo "Effort=0:10")))
 
 ;; (setq org-agenda-custom-commands
 ;;       '(("z" todo ""
@@ -40,7 +43,19 @@
 (setq org-agenda-start-with-log-mode t) ;shows all CLOSED: tasks in agenda view. kinda cool
 (setq org-agenda-skip-scheduled-if-done t)
 (setq org-agenda-restore-windows-after-quit t)
+(setq org-agenda-sticky nil)            ;if t - must refresh agenda to see the changes
 (setq org-agenda-show-future-repeats nil) ;don't show repeating tasks in the future
+(setq org-agenda-tags-column 150)
+(setq org-agenda-use-tag-inheritance nil) ;good, shows in agenda if t,
+                                          ;but doesn't add an actual
+                                          ;tag, in case want to track
+                                          ;finished tasks. must add a
+                                          ;tag manually to each task
+                                          ;instead
+
+
+
+
 
 ;; (setq org-agenda-files (directory-files-recursively "~/Dropbox/documents/org/roam/" "\.org$"))
 (setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
