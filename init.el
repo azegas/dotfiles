890f31a3f31a4a79861d7d7d3bf1af57ed564619
arvydasg <azegaspa@gmail.com>
2022-05-05 20:16:23 +0300

    refactor: renamed file

--- init.el
+++ init.el
@@ -71,18 +71,4 @@
 (load "~/.emacs.d/recipes/web-mode.el")
 ;; (load "~/.emacs.d/recipes/ws-butler.el")
 (load "~/.emacs.d/recipes/yasnippet.el")
-(load "~/.emacs.d/recipes/private.el")
-
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load "~/.emacs.d/recipes/secrets.el")
