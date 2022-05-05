5505b276a615e6463215a09440a0972e071e9c81
arvydasg <azegaspa@gmail.com>
2022-05-05 19:19:31 +0300

    feat: private agenda files list

--- init.el
+++ init.el
@@ -71,6 +71,7 @@
 (load "~/.emacs.d/recipes/web-mode.el")
 ;; (load "~/.emacs.d/recipes/ws-butler.el")
 (load "~/.emacs.d/recipes/yasnippet.el")
+(load "~/.emacs.d/recipes/private.el")
 
 ;;; init.el ends here
 (custom-set-variables
@@ -78,13 +79,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("/home/arvydas/Dropbox/documents/org/roam/20220504202624-smuti_fruti.org" "/home/arvydas/Dropbox/documents/org/roam/20220504180110-pkc.org" "/home/arvydas/Dropbox/documents/org/roam/20220504202102-know_your_area.org" "/home/arvydas/Dropbox/documents/org/roam/20220504192051-emacs.org" "/home/arvydas/Dropbox/documents/org/roam/20220504192335-inbox.org" "/home/arvydas/Dropbox/documents/org/roam/20220504191201-personal.org" "/home/arvydas/Dropbox/documents/org/roam/20220504201142-project_ideas.org" "/home/arvydas/Dropbox/documents/org/roam/20220504200737-citatos.org" "/home/arvydas/Dropbox/documents/org/roam/20220504203435-arvydasg_github_io.org"))))
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'dired-find-alternate-file 'disabled nil)
