a24896e2aad29b2d30685e64f72f6fd97a0d1dcc
arvydasg <azegaspa@gmail.com>
2022-05-23 19:04:21 +0300

    refactor: oi net neklausk ka as cia agendoj dariau.. nebepamenu

--- recipes/org-agenda.el
+++ recipes/org-agenda.el
@@ -9,46 +9,88 @@
 
 ;;; Code:
 
+(setq org-agenda-prefix-format '(
+  ;; (agenda  . " %i %-12:c%?-12t% s") ;; file name + org-agenda-entry-type
+  (agenda  . "  • ")
+  ;; (timeline  . "  % s")
+  ;; (todo  . " %i %-12:c")
+  ;; (tags  . " %i %-12:c")
+  ;; (search . " %i %-12:c")
+  ))
+
+;; System locale to use for formatting time values. Show 2022-02-02 instead of 02/02/22
+(setq system-time-locale "C")         ; Make sure that the weekdays in the
+                                      ; time stamps of your Org mode files and
+                                        ; in the agenda appear in English.
+
+(setq org-agenda-inhibit-startup t)     ; Stop preparing agenda buffers on startup
+(global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-agenda-start-with-log-mode nil) ;shows all CLOSED: tasks in agenda view. kinda cool
+(setq org-agenda-skip-scheduled-if-done t)
+;; (setq org-agenda-prefix-format "%t %s")
+(setq org-agenda-restore-windows-after-quit t)
+(setq org-agenda-sticky nil)                ;allows to open multiple
+                                          ;agenda views at once, but
+                                          ;need a refresh
+(setq org-agenda-show-future-repeats nil) ;don't show repeating tasks in the future
+(require 'org-habit)
+(setq org-agenda-tags-column 90)
+(setq org-habit-graph-column 60)
+(setq org-agenda-use-tag-inheritance nil) ;good, shows in agenda if t,
+                                          ;but doesn't add an actual
+                                          ;tag, in case want to track
+                                          ;finished tasks. must add a
+                                          ;tag manually to each task
+                                          ;instead
+
+;; (setq org-agenda-files (directory-files-recursively "~/Dropbox/documents/org/roam/" "\.org$"))
+(setq org-refile-targets '((org-agenda-files :maxlevel . 2)))
+
 (setq org-agenda-custom-commands
-      '(("ta" "Anywhere" tags-todo "@anywhere-somedaymaybe/!TODO")
-        ("tb" "Buy" tags-todo "@buy-somedaymaybe/!TODO")
-        ("tc" "Calls/asks" tags-todo "@call-somedaymaybe-@repeating/!TODO")
-        ("th" "Home" tags-todo "@home-somedaymaybe-@repeating/!TODO")
-        ("tk" "Komputer" tags-todo "@komputer-somedaymaybe-@repeating/!TODO")
-        ("tr" "Readreview" tags-todo "@readreview-somedaymaybe-@repeating/!TODO")
-        ("tt" "Travel" tags-todo "@travel-somedaymaybe-@repeating/!TODO")
-        ("tp" "Pkc" tags-todo "pkc-@repeating-somedaymaybe/!TODO"
-         ((org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline)))
-         )
-        ("tz" "Panevezys" tags-todo "@pnvz-somedaymaybe-@repeating/!TODO")
-        ("tw" "Waitingfor" tags-todo "@waitingfor-somedaymaybe-@repeating/!TODO")
-        ("ts" "Someday/maybe" tags-todo "-@repeating+somedaymaybe+LEVEL=2" ;show ONLY level 2 heading
-         ((org-agenda-dim-blocked-tasks nil)))
-        ("a" "Agenda"
-         ((agenda ""
-                  ((org-agenda-span 2)))
-          (tags-todo "@anywhere-somedaymaybe|@call-somedaymaybe|@internet-somedaymaybe|@komputer-somedaymaybe/!TODO"
-                     ((org-agenda-overriding-header "Common next actions")
-                      (org-agenda-dim-blocked-tasks 'invisible)
-                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline))))
-          (tags-todo "@home-somedaymaybe/!TODO"
-                     ((org-agenda-overriding-header "Home actions")
-                      (org-agenda-dim-blocked-tasks 'invisible)))
-          (tags-todo "@waitingfor-somedaymaybe/!TODO"
-                     ((org-agenda-overriding-header "Waiting for")
-                      (org-agenda-dim-blocked-tasks 'invisible)))
-          (tags-todo "@pnvz-somedaymaybe/!TODO"
-                     ((org-agenda-overriding-header "Errands Pnvz")
-                      (org-agenda-dim-blocked-tasks 'invisible)))
-          (tags-todo "@readreview-somedaymaybe/!TODO"
-                     ((org-agenda-overriding-header "Read/review")
-                      (org-agenda-dim-blocked-tasks 'invisible)))
-          (tags "/+DONE|+CANCELLED"
-                ((org-agenda-overriding-header "Archivable tasks")
-                 (org-use-tag-inheritance '("project"))))
-          (tags-todo "-@repeating-pkc-@buy-@travel-@anywhere-@call-@internet-@komputer-@home-@readreview-@waitingfor-somedaymaybe/!TODO"
-                ((org-agenda-overriding-header "Contextless tasks")))
-          ))))
+      '(("a" "Simple agenda view"
+         ((agenda "")
+          (todo "IN-PROGRESS" "")))))
+
+;; (setq org-agenda-custom-commands
+;;       '(("ta" "Anywhere" tags-todo "@anywhere-somedaymaybe/!TODO")
+;;         ("tb" "Buy" tags-todo "@buy-somedaymaybe/!TODO")
+;;         ("tc" "Calls/asks" tags-todo "@call-somedaymaybe-@repeating/!TODO")
+;;         ("th" "Home" tags-todo "@home-somedaymaybe-@repeating/!TODO")
+;;         ("tk" "Komputer" tags-todo "@komputer-somedaymaybe-@repeating/!TODO")
+;;         ("tr" "Readreview" tags-todo "@readreview-somedaymaybe-@repeating/!TODO")
+;;         ("tt" "Travel" tags-todo "@travel-somedaymaybe-@repeating/!TODO")
+;;         ("tp" "Pkc" tags-todo "pkc-@repeating-somedaymaybe/!TODO"
+;;          ((org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline)))
+;;          )
+;;         ("tz" "Panevezys" tags-todo "@pnvz-somedaymaybe-@repeating/!TODO")
+;;         ("tw" "Waitingfor" tags-todo "@waitingfor-somedaymaybe-@repeating/!TODO")
+;;         ("ts" "Someday/maybe" tags-todo "-@repeating+somedaymaybe+LEVEL=2" ;show ONLY level 2 heading
+;;          ((org-agenda-dim-blocked-tasks nil)))
+;;         ("a" "Agenda"
+;;          ((agenda ""
+;;                   ((org-agenda-span 2)))
+;;           (tags-todo "@anywhere-somedaymaybe|@call-somedaymaybe|@internet-somedaymaybe|@komputer-somedaymaybe/!TODO"
+;;                      ((org-agenda-overriding-header "Common next actions")
+;;                       (org-agenda-dim-blocked-tasks 'invisible)
+;;                       (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline))))
+;;           (tags-todo "@home-somedaymaybe/!TODO"
+;;                      ((org-agenda-overriding-header "Home actions")
+;;                       (org-agenda-dim-blocked-tasks 'invisible)))
+;;           (tags-todo "@waitingfor-somedaymaybe/!TODO"
+;;                      ((org-agenda-overriding-header "Waiting for")
+;;                       (org-agenda-dim-blocked-tasks 'invisible)))
+;;           (tags-todo "@pnvz-somedaymaybe/!TODO"
+;;                      ((org-agenda-overriding-header "Errands Pnvz")
+;;                       (org-agenda-dim-blocked-tasks 'invisible)))
+;;           (tags-todo "@readreview-somedaymaybe/!TODO"
+;;                      ((org-agenda-overriding-header "Read/review")
+;;                       (org-agenda-dim-blocked-tasks 'invisible)))
+;;           (tags "/+DONE|+CANCELLED"
+;;                 ((org-agenda-overriding-header "Archivable tasks")
+;;                  (org-use-tag-inheritance '("project"))))
+;;           (tags-todo "-@repeating-pkc-@buy-@travel-emacs-@anywhere-@call-@internet-@komputer-@home-@readreview-@waitingfor-somedaymaybe/!TODO"
+;;                 ((org-agenda-overriding-header "Contextless tasks")))
+;;           ))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -109,40 +151,14 @@
         ;; slashes are must to record second quotes
         ;; ("z" "effort stuff" tags-todo "Effort=\"0:10\"")
 
-(defun air-org-skip-subtree-if-priority (priority)
-  "Skip an agenda subtree if it has a priority of PRIORITY.
-PRIORITY may be one of the characters ?A, ?B, or ?C."
-  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
-        (pri-value (* 1000 (- org-lowest-priority priority)))
-        (pri-current (org-get-priority (thing-at-point 'line t))))
-    (if (= pri-value pri-current)
-        subtree-end
-      nil)))
-
-;; System locale to use for formatting time values. Show 2022-02-02 instead of 02/02/22
-(setq system-time-locale "C")         ; Make sure that the weekdays in the
-                                      ; time stamps of your Org mode files and
-                                        ; in the agenda appear in English.
-
-(setq org-agenda-inhibit-startup t)     ; Stop preparing agenda buffers on startup
-(global-set-key (kbd "C-c a") 'org-agenda)
-(setq org-agenda-start-with-log-mode nil) ;shows all CLOSED: tasks in agenda view. kinda cool
-(setq org-agenda-skip-scheduled-if-done t)
-;; (setq org-agenda-prefix-format "%t %s")
-(setq org-agenda-restore-windows-after-quit t)
-(setq org-agenda-sticky nil)                ;allows to open multiple
-                                          ;agenda views at once, but
-                                          ;need a refresh
-(setq org-agenda-show-future-repeats nil) ;don't show repeating tasks in the future
-(setq org-agenda-tags-column 90)
-(setq org-agenda-use-tag-inheritance nil) ;good, shows in agenda if t,
-                                          ;but doesn't add an actual
-                                          ;tag, in case want to track
-                                          ;finished tasks. must add a
-                                          ;tag manually to each task
-                                          ;instead
-
-;; (setq org-agenda-files (directory-files-recursively "~/Dropbox/documents/org/roam/" "\.org$"))
-(setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+;; (defun air-org-skip-subtree-if-priority (priority)
+;;   "Skip an agenda subtree if it has a priority of PRIORITY.
+;; PRIORITY may be one of the characters ?A, ?B, or ?C."
+;;   (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+;;         (pri-value (* 1000 (- org-lowest-priority priority)))
+;;         (pri-current (org-get-priority (thing-at-point 'line t))))
+;;     (if (= pri-value pri-current)
+;;         subtree-end
+;;       nil)))
 
 ;;; agenda.el ends here
