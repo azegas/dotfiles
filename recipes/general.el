ddef00919654cc566c7dc2e45ddd65d34b69dd57
arvydasg <azegaspa@gmail.com>
2022-07-24 21:37:06 +0300

    feat: added duplicate line/paragraph piece of code

--- recipes/general.el
+++ recipes/general.el
@@ -177,15 +177,29 @@
     (switch-to-buffer (other-buffer buf))
     (switch-to-buffer-other-frame buf)))
 
+;; https://rejeep.github.io/emacs/elisp/2010/03/11/duplicate-current-line-or-region-in-emacs.html
+;; for html actually found C-c C-e C from web mode
+;; but will leave this for other modes probably
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
 
-(defun duplicate-line()
-  (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  (yank)
-  (yank)
-  )
-
-(global-set-key (kbd "M-c") 'duplicate-line)
+(global-set-key (kbd "M-c") 'duplicate-current-line-or-region)
 
 ;;; general.el ends here
