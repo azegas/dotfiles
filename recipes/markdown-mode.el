67c3249aed62679b520f48d2c65bc6be47d5718a
arvydasg <azegaspa@gmail.com>
2022-09-13 22:17:32 +0300

    feat: added markdown and yaml modes for hugo

---
+++ recipes/markdown-mode.el
@@ -0,0 +1,9 @@
+;;; markdown-mode.el --- hoping to get some markdown syntax help
+;;; Commentary:
+;; primarily for hugo
+;;; Code:
+
+(use-package markdown-mode
+  :ensure t)
+
+;;; markdown-mode.el ends here
---
+++ recipes/yaml-mode.el
@@ -0,0 +1,9 @@
+;;; yaml-mode.el --- yaml file editing and syntax
+;;; Commentary:
+;; hopefully it will work, need for hugo
+;;; Code:
+
+(use-package yaml-mode
+  :ensure t)
+
+;;; yaml-mode.el ends here
