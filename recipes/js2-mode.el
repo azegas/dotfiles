f29b5830362b15b8a107767dc395282d402fff10
arvydasg <azegaspa@gmail.com>
2022-10-02 10:55:25 +0300

    feat: added some functionality

--- recipes/js2-mode.el
+++ recipes/js2-mode.el
@@ -13,6 +13,7 @@
                 js2-cleanup-whitespace t
                 js2-enter-indents-newline t
                 js2-indent-on-enter-key t
+                js2-strict-missing-semi-warning nil ;remove the damn warning after every line whit no semicolon
                 js2-global-externs (list "window" "module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "jQuery" "$"))
 
   (add-hook 'js2-mode-hook
@@ -23,5 +24,15 @@
 
 (add-hook 'js2-mode-hook
           (lambda () (flycheck-select-checker "javascript-eslint")))
+
+(with-eval-after-load 'js2-mode
+  ;; disabling the hotkeys to hide things
+  (define-key js2-mode-map (kbd "C-c C-e") nil)
+  (define-key js2-mode-map (kbd "C-c C-s") nil)
+  (define-key js2-mode-map (kbd "C-c C-f") nil)
+  (define-key js2-mode-map (kbd "C-c C-t") nil)
+  (define-key js2-mode-map (kbd "C-c C-o") nil)
+  (define-key js2-mode-map (kbd "C-c C-w") nil))
+;; }}
 
 ;;; js2-mode.el ends here
