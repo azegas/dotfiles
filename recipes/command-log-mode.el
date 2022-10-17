3f5f0591d4eafdfdee27f0530d15c2a0d683aa5c
arvydasg <azegaspa@gmail.com>
2022-10-17 06:23:13 +0300

    feat: new plugin

---
+++ recipes/command-log-mode.el
@@ -0,0 +1,11 @@
+;;; command-log-mode.el --- show event history and command history of some or all buffers
+;;; Commentary:
+;; 2022-10-08
+;; run global-command-log-mode
+;; then clm/something
+;;; Code:
+
+(use-package command-log-mode
+  :commands (global-command-log-mode))
+
+;;; command-log-mode.el ends here
