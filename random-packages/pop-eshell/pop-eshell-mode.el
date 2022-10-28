a82ebeaa9d30baf1d88ee3576af68043ec5bdd54
arvydasg <azegaspa@gmail.com>
2022-10-28 16:50:28 +0300

    feat: added some old comments to the new place

--- random-packages/pop-eshell/pop-eshell-mode.el
+++ random-packages/pop-eshell/pop-eshell-mode.el
@@ -153,4 +153,29 @@ otherwise to the parent directory,set by `pop-find-parent-directory' "
 
 (pop-eshell-mode 1)
 
+;; my old code here
+
+;;; pop_eshel.el --- pop_eshell stuff
+;;; Commentary:
+;; [2022-03-16 Tr] [[https://github.com/stanhe/pop-eshell][This]] is the source code. Git clone to .emacsd/elpa then add the config
+;; you see below. With a shortcut of C-c C-e C-e I can now invoke an
+;; eshell buffer at the bottom at the screen. Click the shortcut once
+;; more and it will close. Super convenient!
+;; I use it for django development. I first do M-x pyvenv-activate and
+;; choose a env dir. Then I can open the shell and run python manage.py
+;; runserver. Works like a charm and stays in the background.
+;;; Code:
+
+;; (add-to-list 'load-path "~/Dropbox/src/emacs/random-packages/pop-eshell")
+;; (require 'pop-eshell-mode)
+;; (setq pop-find-parent-directory '(".git" "gradlew")) ;; parent directory should have .git or gradlew file
+;; (pop-eshell-mode 1)
+
+;; Double click to turn on turn off
+
+;; full screen - (define-key map (kbd "C-c C-e C-f") 'fast-eshell-pop)
+;; bottom buffer -  (define-key map (kbd "C-c C-e C-e") 'eshell-pop-toggle)
+
+;;; pop_eshell.el ends here
+
 ;;; pop-eshell-mode end.
