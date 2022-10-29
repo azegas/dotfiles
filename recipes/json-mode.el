87be97562524f37383f4f6e0e1fd1775576acf43
arvydasg <azegaspa@gmail.com>
2022-10-29 19:58:45 +0300

    add json mode for syntax highlighting in json files

---
+++ recipes/json-mode.el
@@ -0,0 +1,10 @@
+;;; json-mode.el --- json mode highlighting and lsp
+;;; Commentary:
+;; any_comments?
+;;; Code:
+
+(use-package json-mode
+  :ensure t
+  :mode ("\\.json"))
+
+;;; json-mode.el ends here
