d584ce3140d5febfa8ff225f35025f3f243a4cf1
arvydasg <azegaspa@gmail.com>
2022-10-29 19:56:08 +0300

    change theme

--- recipes/doom-themes.el
+++ recipes/doom-themes.el
@@ -12,6 +12,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-palenight t))
+  ;; (load-theme 'doom-palenight t)
+  (load-theme 'doom-gruvbox t))
 
 ;;; doom-themes.el ends here
