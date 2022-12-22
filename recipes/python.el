a764eb3a4f98455c162b1775eaab1ac68915833f
arvydasg <azegaspa@gmail.com>
2022-12-22 15:44:25 +0200

    feat: adding a note to myself

--- recipes/python.el
+++ recipes/python.el
@@ -12,6 +12,11 @@
 (add-hook 'elpy-mode-hook (lambda ()
                             (add-hook 'before-save-hook 'elpy-black-fix-code nil t)))
 
+;; IF you can not import modules, says it can not find or w/elfeed
+;; do M-x run-python in DIRED, the location of the files.
+;; then do C-c C-c or C-RET - the modules will load
+;; two hours wasted during my codeacademy first python test... but thanks to this guy:
+;; https://emacs.stackexchange.com/questions/43950/modulenotfound-for-absolute-imports-in-emacs-python-repl/74881#74881
 
 ;; shortcuts
 ;; c-c c-d - pydoc on a method
