4f10e17b556d1ef4c0662a9aa3ccc2016d69ac38
arvydasg <azegaspa@gmail.com>
2022-12-28 13:12:46 +0200

    Revert "feat: include snippets"
    This reverts commit 8a0705569c27fd23dc1c317d8f148891a4cc4176.

--- snippets/emacs-lisp-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `emacs-lisp-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'emacs-lisp-mode
-                     '(("use" "(use-package ${1:package-name}\n  :ensure t$0)" "use" nil nil nil "/home/arvydas/Dropbox/src/emacs/snippets/emacs-lisp-mode/use-package" nil nil)
-                       ("pack" ";;; ${1:name}.el --- ${2:what_it's_for?}\n;;; Commentary:\n;; ${3:any_comments?}\n;;; Code:\n\n$0\n\n;;; ${4:name}.el ends here" "new-package" nil nil nil "/home/arvydas/Dropbox/src/emacs/snippets/emacs-lisp-mode/new-package" nil nil)))
-
-
-;;; Do not edit! File generated at Thu Dec 22 13:08:16 2022
