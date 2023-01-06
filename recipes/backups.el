b6c82d6a2932f3a2d56f68ffb6c399ce575010fd
arvydasg <azegaspa@gmail.com>
2023-01-06 06:46:35 +0200

    turning off backups

--- recipes/backups.el
+++ recipes/backups.el
@@ -7,15 +7,16 @@
 ;; Save backup files to a dedicated directory.
 ;;; Code:
 
-(setq backup-directory-alist '(("." . "~/Dropbox/src/emacs/backups")))
-(setq delete-old-versions -1)
+;; (setq backup-directory-alist '(("." . "~/Dropbox/src/emacs/backups")))
+;; (setq delete-old-versions -1)
 
-;; Make numeric backup versions unconditionally.
-(setq version-control t)
-(setq vc-make-backup-files t)
-(setq auto-save-file-name-transforms '((".*" "~/Dropbox/src/emacs/auto-save-list/" t)))
+;; ;; Make numeric backup versions unconditionally.
+;; (setq version-control t)
+;; (setq vc-make-backup-files t)
+;; (setq auto-save-file-name-transforms '((".*" "~/Dropbox/src/emacs/auto-save-list/" t)))
 
-(setq make-backup-files nil) ; stop creating ~ files
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
 
 ;; (setq make-backup-files nil)
 ;; (setq auto-save-list-file-name  nil)
