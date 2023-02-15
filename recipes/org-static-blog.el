6ff8f82cf7dc497dd300fe6637156b621b4f9ff9
arvydasg <azegaspa@gmail.com>
2023-02-15 05:37:54 +0200

    added google tag manager and google analytics

--- recipes/org-static-blog.el
+++ recipes/org-static-blog.el
@@ -26,7 +26,14 @@
 ;;    and the favicon at
 ;;    ~/projects/blog/static/favicon.ico)
 (setq org-static-blog-page-header
-      "<meta name=\"author\" content=\"Arvydas Gasparavicius\">
+      "<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MC4ZQHP');</script>
+<!-- End Google Tag Manager -->
+<meta name=\"author\" content=\"Arvydas Gasparavicius\">
 <meta name=\"referrer\" content=\"no-referrer\">
 <meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">
 <link href= \"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
@@ -39,6 +46,10 @@
 (setq org-static-blog-page-preamble
 "
 <header>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-MC4ZQHP\"
+height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div class=\"container\">
         <div class=\"subcontainer\">
             <nav class=\"nav\">
