e1858bd62e495eeb6c063e8f642b2e36c2de9e7c
arvydasg <azegaspa@gmail.com>
2023-06-17 17:42:05 +0300

    fix refile targets, set one rule inside the file

--- init.el
+++ init.el
@@ -24,6 +24,9 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(emojify elfeed super-save yaml-mode ws-butler which-key web-mode vterm volatile-highlights use-package undo-tree treemacs shell-pop rg rainbow-delimiters projectile prettier-js powershell plain-org-wiki org-static-blog org-pomodoro org-download multiple-cursors move-text magit lsp-ui lsp-ivy lorem-ipsum impatient-mode hungry-delete helpful goto-chg flycheck expand-region exec-path-from-shell emmet-mode elpy doom-themes doom-modeline dockerfile-mode dired-git-info diff-hl dashboard counsel browse-kill-ring beacon amx all-the-icons-ivy-rich)))
+ '(safe-local-variable-values
+   '((eval setq-local org-refile-targets
+           '((nil :maxlevel . 2))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
