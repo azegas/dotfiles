cb611b810d3ce18f5d2141b818f10bbaf002d4ad
arvydasg <azegaspa@gmail.com>
2023-06-17 17:43:13 +0300

    idk

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(emojify elfeed super-save yaml-mode ws-butler which-key web-mode vterm volatile-highlights use-package undo-tree treemacs shell-pop rg rainbow-delimiters projectile prettier-js powershell plain-org-wiki org-static-blog org-pomodoro org-download multiple-cursors move-text magit lsp-ui lsp-ivy lorem-ipsum impatient-mode hungry-delete helpful goto-chg flycheck expand-region exec-path-from-shell emmet-mode elpy doom-themes doom-modeline dockerfile-mode dired-git-info diff-hl dashboard counsel browse-kill-ring beacon amx all-the-icons-ivy-rich)))
+   '(emojify elfeed super-save yaml-mode ws-butler which-key web-mode vterm volatile-highlights use-package undo-tree treemacs shell-pop rg rainbow-delimiters projectile prettier-js powershell plain-org-wiki org-static-blog org-pomodoro org-download multiple-cursors move-text magit lsp-ui lsp-ivy lorem-ipsum impatient-mode hungry-delete helpful goto-chg flycheck expand-region exec-path-from-shell emmet-mode elpy doom-themes doom-modeline dockerfile-mode dired-git-info diff-hl dashboard counsel browse-kill-ring beacon amx all-the-icons-ivy-rich))
  '(safe-local-variable-values
    '((eval setq-local org-refile-targets
            '((nil :maxlevel . 2))))))
