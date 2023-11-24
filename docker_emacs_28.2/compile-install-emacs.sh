a1b46e86e282b201ccbd25f0da915084893ad573
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-24 08:12:47 +0200

    added emacs 28.2 dockerfile
    useful for testing out emacs on the fly

---
+++ docker_emacs_28.2/compile-install-emacs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev libgnutls28-dev -y
+
+wget http://ftp.gnu.org/gnu/emacs/emacs-28.2.tar.gz
+tar xvzf emacs-28.2.tar.gz
+
+cd emacs-28.2
+./configure
+make
+sudo make install
