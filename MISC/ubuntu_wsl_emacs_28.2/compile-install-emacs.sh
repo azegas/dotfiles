6b7ff85a01e9adcce724dfdbaf5a9b7e5d605467
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-24 17:54:37 +0200

    cleanup after installation

--- MISC/ubuntu_wsl_emacs_28.2/compile-install-emacs.sh
+++ MISC/ubuntu_wsl_emacs_28.2/compile-install-emacs.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Create a directory for Emacs compilation
+mkdir -p ~/emacs_build
+cd ~/emacs_build || exit
+
 # STUFF NEEDED FOR BUILDING EMACS
 sudo apt install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev libgnutls28-dev -y
 
-cd ~
-
+# Download Emacs source code
 wget http://ftp.gnu.org/gnu/emacs/emacs-28.2.tar.gz
 tar xvzf emacs-28.2.tar.gz
 
-cd emacs-28.2
+cd emacs-28.2 || exit
 ./configure
 make
 sudo make install
+
+# Remove installation files
+# cd ~/emacs_build && rm -rf emacs-28.2 emacs-28.2.tar.gz
