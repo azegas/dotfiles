8c61378f09d560005442c64246d596bbd51213a7
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-25 09:50:24 +0200

    remove early-init.el, update init.el
    will care about shaving off seconds leter.
    
    Maybe better make emacs run as daemon and that's it, won't have to care about 3 seconds load time

--- early-init.el
+++
@@ -1,5 +0,0 @@
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html
-
-;; "customizations take effect during Emacs startup earlier than the normal init file is processed"
-
-;; Documentation above says that this file is not really intended to be used for graphic changes, but I will add graphic changes here only, note sure what else I can add. Garbage collection maybe.. But later.
--- init.el
+++ init.el
@@ -1,3 +1,28 @@
-(require 'package)
+;;; -*- lexical-binding: t -*-
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/"))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Make sure we install any packages that aren't on the system (no need to write :ensure t anymore)
+(setq use-package-always-ensure t)
+
+;; delays loading the package until one of its declared features is needed. This can help in speeding up Emacs startup time by loading packages only when necessary
+(setq use-package-always-defer t)
+
+;; Let's load our config
+(dolist (element '("key-bindings.el"
+                   "behaviour.el"
+                   "magit.el"))
+
+  (measure-time (load (concat "~/.emacs.d/config/" element))))
+
+(add-hook 'server-after-make-frame-hook
+	  (lambda ()
+	    (message (concat "emacs uptime: " (emacs-uptime)))))
+
+(message (concat (format "Emacs took %.2f seconds to start" (float-time (time-subtract after-init-time before-init-time)))
+		 (if (fboundp 'native-compile-async) " With native compiler!")))
