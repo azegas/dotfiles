668be284faf02b609ebbd0cb45ce6a6274ebb6a7
arvydasg <azegaspa@gmail.com>
2023-11-28 16:17:18 +0200

    give me those suggestions quickier

--- config/which-key.el
+++ config/which-key.el
@@ -9,7 +9,6 @@
   :init
   (setq which-key-separator " ")
   (setq which-key-prefix-prefix "+")
-  (setq which-key-idle-delay 1)
-  ;; (setq which-key-idle-delay 0.2)
+  (setq which-key-idle-delay 0.2)
   :config
   (which-key-mode 1))
