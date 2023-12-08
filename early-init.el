35aee0e84a45c960ebc95c3218f82102d0fff80e
arvydasg <azegaspa@gmail.com>
2023-12-08 02:36:11 +0200

    BIG REFACTOR - org-src-preserve-indentation nil

--- early-init.el
+++ early-init.el
@@ -1,8 +1,8 @@
 (dolist (mode
-   '(tool-bar-mode                ; No toolbars, more room for text
-     scroll-bar-mode              ; No scroll bars either
-     menu-bar-mode                ; No menu bar as well
-     blink-cursor-mode))          ; Disable blinking cursor
+         '(tool-bar-mode                ; No toolbars, more room for text
+           scroll-bar-mode              ; No scroll bars either
+           menu-bar-mode                ; No menu bar as well
+           blink-cursor-mode))          ; Disable blinking cursor
   (funcall mode 0))
 
 ;; (add-to-list 'default-frame-alist '(internal-border-width . 24))
