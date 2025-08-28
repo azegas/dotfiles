edaddf9c75253f81e1e0aff2ebe30f71be48c3b3
Arvydas <azegaspa@gmail.com>
2025-08-28 16:54:50 +0300

    start quotes projects

--- raspberry/start-docker-projects.sh
+++ raspberry/start-docker-projects.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd /home/arvydas/GIT/monitoring && /usr/bin/docker compose up -d
+cd /home/arvydas/GIT/QUOTES.WEB && /usr/bin/docker compose -f docker-compose-pi.yml up --build -d
+cd /home/arvydas/GIT/QUOTES.API&& /usr/bin/docker compose -f docker-compose-pi.yml up --build -d
 
 echo "All Docker projects started."
